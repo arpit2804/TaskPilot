d7f5c5d28bf8ef4ee4677c94df3aeadb8cd141a6
arpit2804 <guptaji1004@gmail.com>
2024-04-22 05:12:06 +0530

    tasks controllers

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,25 +1,83 @@
 const Task = require('../models/tasks');
 const {StatusCodes} = require('http-status-codes');
-
-//const date = new Date(2024,3,22,15,30,0);
+const {Op} = require('sequelize');
+const {BadRequestError,NotFoundError} = require('../errors');
 
 const getTasks = async(req,res) => {
-    res.send("hello get");
+    const {userId} = req.user;
+    const tasks = await Task.findAll({
+        where: {
+            UserId : userId
+        },
+        order: [
+            ['endDate','ASC']
+        ]
+    });
+
+    res.status(StatusCodes.OK).json({tasks, count:tasks.length});
+}
+
+const getOneTask = async(req,res) => {
+    const {
+    user: { userId },
+    params: { id: taskId },
+  } = req
+
+  const task = await Task.findOne({
+   where: {
+    [Op.and] : [{UserId:userId},{taskid:taskId}],
+   },
+  });
+
+  if (!task) {
+    throw new NotFoundError(`No job with id ${taskId}`);
+  }
+  res.status(StatusCodes.OK).json({ task });
 }
 
 
 const createTasks = async(req,res) =>{
-    const {UserId} = req.user;
+    const {userId} = req.user;
     const{date,month,year} = req.body;
-    const endDate = new Date(date,month,year);
+    const endDate = new Date(Number(year),Number(month),Number(date),23,59,0);
 
-    const task = await Task.create({...req.body,endDate,UserId});
+    const task = await Task.create({...req.body,endDate,UserId:userId});
 
     res.status(StatusCodes.CREATED).json({task});
 }
 
 const editTasks = async(req,res) => {
-    res.send("hello patch");
+    const {
+        user:{userId},
+        body:{description,status},
+        params : {id :taskId}
+    } =req;
+
+    if(description){
+        if(description === ''){
+            throw new BadRequestError('Please enter valid description');
+        }
+     const task = await Task.findByPk(taskId);
+     if(!task) throw new BadRequestError(`No task with id:${taskId}`);
+
+     task.description = description;
+     await task.save();
+     return res.status(StatusCodes.OK).json({task});
+
+    }
+    if(status){
+        if(status === ''){
+            throw new BadRequestError('Invalid Status');
+        }
+
+        const task = await Task.findByPk(taskId);
+        if(!task) throw new BadRequestError(`No task with id:${taskId}`);
+
+        task.status = status;
+        await task.save();
+        return res.status(StatusCodes.OK).json({task});
+    }
+    return res.status(StatusCodes.OK).json({updation:false});
 }
 
 const deleteTasks = async(req,res) =>{
@@ -28,6 +86,7 @@ const deleteTasks = async(req,res) =>{
 
 module.exports = {
     getTasks , 
+    getOneTask,
     createTasks,
     editTasks,
     deleteTasks,

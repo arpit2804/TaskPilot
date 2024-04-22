0f7307245e3ee5d29d46b39a391378fc3b972ac3
Rishi Upadhyay <rupadhyay2674@gmail.com>
2024-04-23 02:04:02 +0530

    signup auth integrated

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/taskpilot.png'
 import banner from '../assets/Login_banner.png'
+import { FaRegUser } from 'react-icons/fa';
 
 export default function Signup() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [password, setPassword] = useState("");
+
+
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault()
+        const data = {
+            name : name,
+            phoneNumber: phone,
+            email: email,
+            password: password,
+        }
+        try {
+            
+            await fetch('http://localhost:3000/api/v1/auth/register',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json;charset=utf-8',
+                },
+                body: JSON.stringify(data),
+            })
+            .then(res=>res.json()).then((result)=>{
+                console.log(result)
+                window.href.replace('/')
+            })
+        } catch (error) {
+            console.log("error",error);
+        }
+    }
+
+
   return (
     <div className="h-screen bg-black">
           <div className="flex justify-between">
@@ -24,27 +61,42 @@ export default function Signup() {
           <div className="bg-[#1f1f1f] h-[75%] my-[3rem] mx-[12rem] rounded-2xl flex flex-col items-center">
             <div className="text-[2.7rem] text-white mt-2">SIGNUP</div>
             <div className="flex items-center justify-center w-full my-7">
+            <form
+            onSubmit={handleSubmit} 
+            >
               <div className="flex flex-col items-center w-[50%] justify-around h-[100%] border-r-2 border-[#A1A1A1] px-4 py-6 pt-14">
                 <div className="flex flex-col w-full  items-center justify-around h-[80%]">
                   <input
                     placeholder="Name"
                     type='text'
+                    onChange={(e)=>{
+                        setName(e.target.value)
+                    }} 
                     className="w-[75%] bg-[#1f1f1f] text-[#A1A1A1] border-0 border-b-2 border-[#A1A1A1] placeholder:text-[#A1A1A1] text-xl placeholder:text-2xl placeholder:text-center focus:outline-none"
                   />
                   <div className="w-full flex flex-col justify-center items-center ">
                     <input
                       placeholder="johndoe@example.com"
                       type = "email"
+                      onChange={(e)=>{
+                        setEmail(e.target.value)
+                    }} 
                       className="w-[75%] bg-[#1f1f1f] text-[#A1A1A1] border-0 border-b-2 border-[#A1A1A1] placeholder:text-[#A1A1A1] text-xl placeholder:text-2xl placeholder:text-center mt-8 focus:outline-none"
                     />
                      <input
                       placeholder="Phone "
-                      type = 'phone'
+                      type = 'text'
+                      onChange={(e)=>{
+                        setPhone(e.target.value)
+                    }} 
                       className="w-[75%] bg-[#1f1f1f] text-[#A1A1A1] border-0 border-b-2 border-[#A1A1A1] placeholder:text-[#A1A1A1] text-xl placeholder:text-2xl placeholder:text-center mt-8 focus:outline-none"
                     />
                      <input
                       placeholder="Password"
                       type='password'
+                      onChange={(e)=>{
+                        setPassword(e.target.value)
+                    }} 
                       className="w-[75%] bg-[#1f1f1f] text-[#A1A1A1] border-0 border-b-2 border-[#A1A1A1] placeholder:text-[#A1A1A1] text-xl placeholder:text-2xl placeholder:text-center mt-8 focus:outline-none"
                     />
                     <div className=" text-white w-[75%] mt-4">
@@ -57,11 +109,14 @@ export default function Signup() {
                   </div>
                 </div>
                 <div className="h-[20%] flex flex-col justify-end mt-4">
-                  <button className="py-1 px-12 text-black font-semibold bg-gradient-to-r from-[#ff6cab] via-[#BD69D2] to-[#7766FD] rounded-3xl text-xl hover:scale-110 duration-300">
+                  <button 
+                  type='submit'
+                  className="py-1 px-12 text-black font-semibold bg-gradient-to-r from-[#ff6cab] via-[#BD69D2] to-[#7766FD] rounded-3xl text-xl hover:scale-110 duration-300">
                     Signup
                   </button>
                 </div>
               </div>
+              </form>
               <div className="w-[50%] flex justify-center items-center">
                 <img src={banner} />
               </div>
